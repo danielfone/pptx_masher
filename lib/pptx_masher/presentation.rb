73ae66fe1ff583ade61eced1f63b37a4c76412fb
Daniel Fone <daniel@fone.net.nz>
2015-05-27 16:00:23 +1200

    Working slide duplication

--- lib/pptx_masher/presentation.rb
+++ lib/pptx_masher/presentation.rb
@@ -1,6 +1,7 @@
 require 'zip/filesystem'
 require 'fileutils'
 require 'tmpdir'
+require 'securerandom'
 
 module PPTXMasher
   class Presentation
@@ -8,7 +9,7 @@ module PPTXMasher
 
     def self.load(path)
       warn "Extract into separate class"
-      out_path = File.join Dir.tmpdir, "pptx_masher", "extract_#{Time.now.strftime("%Y-%m-%d-%H%M%S")}"
+      out_path = File.join Dir.tmpdir, "pptx_masher", SecureRandom.uuid
       Zip::File.open(path) do |zip_file|
         zip_file.each do |f|
           f_path = File.join(out_path, f.name)
@@ -27,6 +28,18 @@ module PPTXMasher
       @slides ||= Hash.new { |h, i| h[i] = Slide.new(self, i) }
     end
 
+    def add_slide(slide)
+      n = slide_count + 1
+      new_slide = slides[n]
+      FileUtils.copy slide.rels_path, new_slide.rels_path
+      FileUtils.copy slide.slide_xml_full_path, new_slide.slide_xml_full_path
+      # Copy media
+      new_slide.insert_content_type
+      new_slide.insert_rels
+      new_slide.insert_meta
+      new_slide
+    end
+
     def save(path)
       warn "Extract into separate class"
       Zip::File.open(path, Zip::File::CREATE) do |zip_file|
@@ -43,6 +56,10 @@ module PPTXMasher
       true
     end
 
+    def slide_count
+      Dir.glob(tmp_dir+"/ppt/slides/slide*.xml").count
+    end
+
   end
 
   class Slide
@@ -54,20 +71,60 @@ module PPTXMasher
     end
 
     def slide_xml_path
-      @slide_xml_path ||= "#{presentation_path}/ppt/slides/slide#{@number}.xml"
+      @slide_xml_path ||= "/ppt/slides/slide#{@number}.xml"
+    end
+
+    def slide_xml_full_path
+      @slide_xml_full_path ||= "#{presentation_path}#{slide_xml_path}"
+    end
+
+    def rels_path
+      @rels_path ||= "#{presentation_path}/ppt/slides/_rels/slide#{number}.xml.rels"
     end
 
     def replace_text(pattern, replacement)
-      text = File.read(slide_xml_path)
-      new_contents = text.gsub pattern, replacement
-      File.open(slide_xml_path, "w") {|f| f.puts new_contents }
+      gsub_file slide_xml_full_path, pattern, replacement
     end
 
     def replace_media(dest, src)
-      FileUtils.copy src, "#{presentation_path}/ppt/media/#{dest}"
+      extension = dest.split('.').last
+      new_name = "#{SecureRandom.uuid}.#{extension}"
+      gsub_file rels_path, dest, new_name
+      FileUtils.copy src, "#{presentation_path}/ppt/media/#{new_name}"
+    end
+
+    def insert_content_type
+      append_xml "#{presentation_path}/[Content_Types].xml", 'Types',
+        %Q[<Override PartName="#{slide_xml_path}" ContentType="application/vnd.openxmlformats-officedocument.presentationml.slide+xml"/>]
+    end
+
+    def insert_rels
+      append_xml "#{presentation_path}/ppt/_rels/presentation.xml.rels", 'Relationships',
+        %Q[<Relationship Id="#{id}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slide" Target="slides/slide#{number}.xml"/>]
+    end
+
+    def insert_meta
+      append_xml "#{presentation_path}/ppt/presentation.xml", 'p:sldIdLst',
+        %Q[<p:sldId id="257" r:id="#{id}"/>]
+    end
+
+    def id
+      @id ||= "rId#{number+1000}"
     end
 
   private
+
+    def append_xml(file, node, xml)
+      closing_tag = "</#{node}>"
+      xml += closing_tag
+      gsub_file file, closing_tag, xml
+    end
+
+    def gsub_file(path, pattern, replacement)
+      text = File.read(path)
+      text.gsub! pattern, replacement
+      File.open(path, "w") {|f| f.puts text }
+    end
 
     def presentation_path
       @presentation_path ||= presentation.tmp_dir
--- spec/pptx_masher_spec.rb
+++ spec/pptx_masher_spec.rb
@@ -6,6 +6,15 @@ describe PPTXMasher do
   end
 
   it 'does something useful' do
-    expect(false).to eq(true)
+    output_path = "/Users/danielfone/Desktop/cojoined.pptx"
+    File.delete output_path if File.exists? output_path
+    template = PPTXMasher::Presentation.load "/Users/danielfone/Desktop/stasaph_rate_card.pptx"
+    s = template.slides[1]
+    s2 = template.add_slide s
+    s2.replace_text '[TITLE]', "First update"
+    s2.replace_media 'image6.jpeg', '/Users/danielfone/Desktop/AK00101-1.jpg'
+    s3 = template.add_slide s
+    s3.replace_text '[TITLE]', "Second update"
+    template.save output_path
   end
 end
